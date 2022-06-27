296499dc035fd12ce0e1f127c10974a7774f8938
Sébastien Chopin <seb@nuxtjs.com>
2022-06-27 18:57:56 +0200

    docs: update features

--- components/content/List.vue
+++ components/content/List.vue
@@ -5,14 +5,14 @@ const { flatUnwrap } = useUnwrap()
 defineProps({
   icon: {
     type: String,
-    default: 'heroicons-outline:badge-check'
+    default: 'ph:check-circle-duotone'
   }
 })
 </script>
 
 <template>
   <ul class="p-0">
-    <li v-for="(item, index) of flatUnwrap($slots.default(), ['ul'])" :key="index" class="flex space-x-2">
+    <li v-for="(item, index) of flatUnwrap($slots.default(), ['ul'])" :key="index" class="flex pl-0 space-x-2">
       <Icon :name="icon" class="flex-shrink-0 w-6 h-6 mt-1 text-emerald-500" />
       <span><Markdown :use="() => item" unwrap="li" /></span>
     </li>
