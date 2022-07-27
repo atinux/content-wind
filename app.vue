13e1764c8ac2739037eced2bab3baafa47c5d02e
Sébastien Chopin <seb@nuxtjs.com>
2022-07-27 17:10:49 +0200

    chore: update

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="sm:py-10">
-    <Html lang="en" />
-    <Meta property="og:image" content="/cover.jpg" />
-    <Meta name="twitter:card" content="summary_large_image" />
-    <AppNavbar />
+  <ContentWind>
     <NuxtPage />
-  </div>
+  </ContentWind>
 </template>
-
-<style lang="postcss">
-body {
-  @apply bg-primary-50 dark:bg-primary-900;
-}
-</style>
---
+++ components/ContentWind.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sm:py-10">
+    <Html lang="en" />
+    <Meta property="og:image" content="/cover.jpg" />
+    <Meta name="twitter:card" content="summary_large_image" />
+    <AppNavbar />
+    <slot />
+  </div>
+</template>
+
+<style lang="postcss">
+body {
+  @apply bg-primary-50 dark:bg-primary-900;
+}
+</style>
