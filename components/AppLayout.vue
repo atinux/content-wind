87b9873b691aa202f5d500af98b7d938139224db
Sébastien Chopin <seb@nuxtjs.com>
2022-08-03 02:39:48 +0200

    chore: use nuxt edge (#16)
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- components/AppLayout.vue
+++ components/AppLayout.vue
@@ -3,6 +3,7 @@
     <Html lang="en" />
     <Meta property="og:image" content="/cover.jpg" />
     <Meta name="twitter:card" content="summary_large_image" />
+    <AppLoadingBar />
     <AppNavbar />
     <slot />
   </div>
