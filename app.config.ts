67e91252ac017006089fab6173550deab95c6c79
Sébastien Chopin <seb@nuxtjs.com>
2022-10-24 17:50:20 +0200

    feat: add cover as global option

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,5 @@
 export default defineAppConfig({
+  cover: '/cover.jpg',
   socials: {}
 })
 
--- components/AppLayout.vue
+++ components/AppLayout.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+const { cover } = useAppConfig()
+</script>
+
 <template>
   <div class="sm:pt-6 sm:pb-10">
     <Html lang="en" />
-    <Meta property="og:image" content="/cover.jpg" />
+    <Meta property="og:image" :content="cover" />
     <Meta name="twitter:card" content="summary_large_image" />
     <AppLoadingBar />
     <AppNavbar />
