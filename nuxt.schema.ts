a75f304e9bd3fed3a94b3db9c847c1cc5c9699f5
Sébastien Chopin <seb@nuxtjs.com>
2022-11-29 17:28:58 +0100

    feat: leverage nuxt.schema for app.config definition

---
+++ nuxt.schema.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfigSchema({
+  appConfig: {
+    /** Default OG image path */
+    cover: '/cover.jpg',
+    socials: {
+      /** Twitter handle */
+      twitter: '',
+      /** Github repository name */
+      github: ''
+    }
+  }
+})
