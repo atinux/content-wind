e849c3357456e8cdff92ab75f0a81465b7cc4488
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-08 11:19:27 +0800

    feat: list icon class

--- components/content/List.vue
+++ components/content/List.vue
@@ -6,6 +6,10 @@ defineProps({
   icon: {
     type: String,
     default: 'ph:check-circle-duotone'
+  },
+  iconClass: {
+    type: String,
+    default: ''
   }
 })
 </script>
@@ -13,7 +17,7 @@ defineProps({
 <template>
   <ul class="p-0">
     <li v-for="(item, index) of flatUnwrap($slots.default(), ['ul'])" :key="index" class="flex pl-0 space-x-2 my-1 items-center">
-      <Icon :name="icon" class="flex-shrink-0 w-6 h-6 mt-1 text-emerald-500" />
+      <Icon :name="icon" class="flex-shrink-0 w-6 h-6 mt-1 text-emerald-500" :class="iconClass" />
       <span><ContentSlot :use="() => item" unwrap="li" /></span>
     </li>
   </ul>
