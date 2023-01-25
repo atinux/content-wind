6f04324ce1c31e492280337f2b3089292022a463
Sébastien Chopin <seb@nuxtjs.com>
2023-01-25 14:54:48 +0100

    chore: update deps

--- app.config.ts
+++ app.config.ts
@@ -3,5 +3,11 @@ export default defineAppConfig({
   socials: {
     twitter: '',
     github: ''
+  },
+  nuxtIcon: {
+    aliases: {
+      'dark-mode': 'ph:moon-bold',
+      'light-mode': 'ph:sun-bold'
+    }
   }
 })
